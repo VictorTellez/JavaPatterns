40c65543b62072ae1f40841a2bd0b566ababf6bb
victor_tellez <victorm.tellezm@gmail.com>
2015-11-10 20:31:29 +0100

    Upload project #1

---
+++ app/src/main/java/com/vtellez/apps/javapatterns/MainActivity.java
@@ -0,0 +1,13 @@
+package com.vtellez.apps.javapatterns;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
