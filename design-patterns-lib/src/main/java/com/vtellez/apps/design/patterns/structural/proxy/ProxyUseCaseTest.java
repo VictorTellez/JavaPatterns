ad5f61ffbf3321bcd842fe4f32a2e07e5480aaf1
victor_tellez <victorm.tellezm@gmail.com>
2015-12-08 16:16:40 +0100

    Implement the proxy design pattern.

---
+++ design-patterns-lib/src/main/java/com/vtellez/apps/design/patterns/structural/proxy/ProxyUseCaseTest.java
@@ -0,0 +1,16 @@
+package com.vtellez.apps.design.patterns.structural.proxy;
+
+import org.junit.Test;
+
+/**
+ * This the test of the proxy pattern.
+ *
+ * Created by Victor Tellez on 08/12/2015.
+ */
+public class ProxyUseCaseTest {
+    @Test
+    public void test() {
+        String[] args = {};
+        ProxyUseCase.main(args);
+    }
+}
